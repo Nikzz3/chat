7a798b77dc0150acf5e43becbe43cc689dc36d1c
Nick Zimmermann <nick.zimmermann@bluewin.ch>
2023-03-26 20:11:26 +0200

    remove output

--- .output/server/chunks/app-styles.d94b2c89.mjs
+++
@@ -1,6 +0,0 @@
-const app_vue_vue_type_style_index_0_lang = "body{font-family:Arial,sans-serif;margin:0;padding:0}.chat-container{margin:20px 20px 0;max-width:600px}.chat-header{margin-bottom:20px;text-align:center}.chat-messages{max-height:400px;overflow-y:scroll}.message{border-radius:5px;margin-bottom:10px;padding:10px}.received{background-color:#f1f0f0;clear:both;float:left}.sent{background-color:#dcf8c6;clear:both;float:right}.timestamp{color:#888;display:block;font-size:12px;margin-top:5px}.chat-input{display:flex;margin-top:20px;width:100%}.chat-input input[type=text]{border:1px solid #ccc;border-radius:5px;float:left;padding:10px;width:80%}.chat-input button{background-color:#4caf50;border:none;border-radius:0 5px 5px 0;color:#fff;cursor:pointer;float:left;padding:10px 20px;width:20%}";
-
-const appStyles_d94b2c89 = [app_vue_vue_type_style_index_0_lang];
-
-export { appStyles_d94b2c89 as default };
-//# sourceMappingURL=app-styles.d94b2c89.mjs.map
--- .output/server/chunks/styles.mjs
+++
@@ -1,9 +0,0 @@
-const interopDefault = r => r.default || r || [];
-const styles = {
-  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": () => import('./error-404-styles.a5c3f351.mjs').then(interopDefault),
-  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": () => import('./error-500-styles.6b5b5ff2.mjs').then(interopDefault),
-  "app.vue": () => import('./app-styles.d94b2c89.mjs').then(interopDefault)
-};
-
-export { styles as default };
-//# sourceMappingURL=styles.mjs.map
